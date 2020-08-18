2a12566970f6332f2ede3ef2a2511cd2a280d7f3
Beatrice Beauperin <beauperin.beatrice@gmail.com>
2020-08-18 12:19:56 +0200

    received a error message from API if response = 500, or user's data when response = 200

--- apis/users/index.js
+++ apis/users/index.js
@@ -2,6 +2,7 @@
 const express = require(`express`);
 const User = require(`../../models/User`);
 const api = express.Router();
+const { NotFoundError } = require('objection');
 
 api.get(`/`, async (req, res) => {
   const users = await User.query();
@@ -24,17 +25,38 @@ api.post(`/`, async (req, res) => {
 
 /* API TEST*/
 api.post(`/`, async (req, res) => {
-  const user = await User.query()
-  .insertAndFetch({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-  }, 
-      function (err) {
-        res.json(user);
-      }
-  );
+  try {
+    const user = await User.query()
+    .insertAndFetch({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    })
+        // function errorHandler(err, res) {
+        //   if (err) return res.status(500).send("there was a problem with the user");
+        //   //res.json(user);
+        // }
+    // .timeout({
+    //   message: `il y a une erreur`,
+    //   type: `Custom type`
+    // })
+    // .connection({
+    //   message: `il y a une erreur de connection`
+    // });
+
+    res.json(user);
+  } catch (err) {
+
+  // if(err instanceof NotFoundError) {
+  //   return res.status(404).json({
+  //     error: 'NotFoundError',
+  //     message: `industry not found`,
+  //   });
+  // }
+
+  return res.status(500).send("Vous avez rencontré une erreur");
+}
   res.json(user);
 });
 
