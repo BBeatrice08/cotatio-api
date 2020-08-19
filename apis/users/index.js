af78da03900158500e411d855f4ca695f7463965
Beatrice Beauperin <beauperin.beatrice@gmail.com>
2020-08-19 11:36:13 +0200

    a bit of cleaning

--- apis/users/index.js
+++ apis/users/index.js
@@ -9,21 +9,7 @@ api.get(`/`, async (req, res) => {
   res.json(users);
 });
 
-/* API OK
-api.post(`/`, async (req, res) => {
-  const user = await User.query()
-  .insertAndFetch({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
-  res.json(user);
-});
-*/
-
-/* API TEST*/
+/* API TEST OK*/
 api.post(`/`, async (req, res) => {
   try {
     const user = await User.query()
@@ -33,27 +19,11 @@ api.post(`/`, async (req, res) => {
       email: req.body.email,
       password: req.body.password,
     })
-        // function errorHandler(err, res) {
-        //   if (err) return res.status(500).send("there was a problem with the user");
-        //   //res.json(user);
-        // }
-    // .timeout({
-    //   message: `il y a une erreur`,
-    //   type: `Custom type`
-    // })
-    // .connection({
-    //   message: `il y a une erreur de connection`
-    // });
+    //.returning('email');
+    .select('user');
 
     res.json(user);
   } catch (err) {
-
-  // if(err instanceof NotFoundError) {
-  //   return res.status(404).json({
-  //     error: 'NotFoundError',
-  //     message: `industry not found`,
-  //   });
-  // }
 
   return res.status(500).send("Vous avez rencontré une erreur");
 }
