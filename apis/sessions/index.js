1e23b511404c24b1a2d03d7003828401bf018009
Bea <beauperin.beatrice@gmail.com>
2020-09-02 17:13:37 +0200

    cleaning

--- apis/sessions/index.js
+++ apis/sessions/index.js
@@ -15,17 +15,6 @@ api.get(`/`, async (req, res) => {
   res.json(sessions);
 });
 
-/*api.post(`/`, async (req, res) => {
-  const session = await Session.query()
-  .insertAndFetch({
-    date: req.body.date,
-    user_email: req.body.user_email,
-    user_password: req.body.user_password
-  });
-
-  res.json(session);
-});*/
-
 // il  s'agit d'un faux post. Il s'agit finalement d'un get pour récupérer les données renvoyées
 // par le serveur
 //pas possible de réaliser des vérifications dans l'API. On reçoit un fichier JSON non découpable
@@ -47,7 +36,6 @@ api.post(`/`, async (req, res) => {
 
   res.json(users);
 });
-
 
 
 module.exports = api;
