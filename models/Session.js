8a66fe3317803b8253ef440d04a37618e077c7cc
Bea <beauperin.beatrice@gmail.com>
2020-10-21 14:57:40 +0200

    file models/Session.jsfile models/Session.js remove by error. It has been recreated

---
+++ models/Session.js
@@ -0,0 +1,24 @@
+
+// const { Model } = require(`objection`);
+
+// class Session extends Model {
+//   static get tableName() {
+//     return `session`;
+//   }
+
+//   static get jsonSchema() {
+//     return {
+//       type: `object`,
+
+//       properties: {
+//         id: { type: `integer` },
+//         date: { type: `timestamp` },
+//         user_email: { type: `string` },
+//         user_password: { type: `string` },
+//       },
+//     };
+//   }
+
+// }
+
+// module.exports = Session;
